ce01ceb8d1812df4cc13549e10a2bc00fecebc1d
cph <stegben.benjamin@gmail.com>
2016-12-08 20:33:22 +0800

    12/8 update

--- run.py
+++ run.py
@@ -1,9 +1,15 @@
 import pandas as pd
-
+from sklearn.linear_model import Ridge
 
 def main():
     df_train = pd.read_csv('./raw_data/train.csv')
     df_test = pd.read_csv('./raw_data/test.csv')
+
+    import ipdb; ipdb.set_trace()
+    x_train = df_train.drop(['Id', 'SalePrice'], axis=1).values
+    y_train = df_train['SalePrice'].values
+    x_test = df_test.drop(['Id'], axis=1).values
+    id_test = df_test['Id']
 
     import ipdb; ipdb.set_trace()
 
